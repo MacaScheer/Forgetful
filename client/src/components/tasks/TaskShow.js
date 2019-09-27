da8cf3add6108e27b4697a18b1dfda940e18eb9c
Michael_Scheer <macas.scheer@gmail.com>
2019-09-27 16:57:12 -0700

    fix fetchPolicy to nocache so task show page always updates between tasks

--- client/src/components/tasks/TaskShow.js
+++ client/src/components/tasks/TaskShow.js
@@ -15,46 +15,70 @@ import StartDateDetail from "../details/StartDateDetail";
 
 const { FETCH_TASK } = queries;
 
+class TaskShow extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   id: props.taskId
+    // };
+  }
+  // shouldComponentUpdate(nextProps) {
+  //   if (nextProps.taskId !== this.props.taskId) {
+  //     this.setState({
+  //       id: nextProps.taskId
+  //     });
+  //     console.log("nextProps", nextProps.taskId);
+  //     console.log("props.taskId", this.props.taskId);
+  //     return true;
+  //   }
+  //   return false
+  // }
 
+  render() {
+    const id = this.props.taskId;
 
-const TaskShow = props => {
-  return (
-    <Query query={FETCH_TASK} variables={{ Id: props.taskId }}>
-      {({ loading, error, data }) => {
-        if (loading) return <p> Loading...</p>;
-        if (error) return `Error! ${error.message}`;
-        let tags = data.task.tags;
-        
-        // debugger;
-        return (
-          <div className="task-show-content">
-            <div className="task-show">
-              <NameDetail id={data.task._id} name={data.task.name} />
-              <div className="task-show-info">
-                <StartDateDetail id={data.task._id} start_date={data.task.start_date} />
-                <DueDateDetail
-                  id={data.task._id}
-                  due_date={data.task.due_date}
-                />
+    return (
+      <Query query={FETCH_TASK} variables={{ Id: id }} fetchPolicy={"no-cache"}>
+        {({ loading, error, data }) => {
+          if (loading) return <p> Loading...</p>;
+          if (error) return `Error! ${error.message}`;
+          let tags = data.task.tags;
 
-                
-                <ListDetail id={data.task._id} list={data.task.list} />
-                <TagDetail id={data.task._id} tags={data.task.tags} />
-                {/* <PriorityDetail
+          // debugger;
+          return (
+            <div className="task-show-content">
+              <div className="task-show">
+                <NameDetail id={data.task._id} name={data.task.name} />
+                <div className="task-show-info">
+                  <StartDateDetail
+                    id={data.task._id}
+                    start_date={data.task.start_date}
+                  />
+                  <DueDateDetail
+                    id={data.task._id}
+                    due_date={data.task.due_date}
+                  />
+
+                  <ListDetail id={data.task._id} list={data.task.list} />
+                  <TagDetail id={data.task._id} tags={data.task.tags} />
+                  {/* <PriorityDetail
                   id={data.task._id}
                   priority={data.task.priority}
                 /> */}
-                {/* <RepeatDetail id={data.task._id} repeat={data.task.repeat} /> */}
-                <LocationDetail id={data.task._id} location={data.task.location}/>
-                <BodyDetail id={data.task._id} body={data.task.body} />
-                
+                  {/* <RepeatDetail id={data.task._id} repeat={data.task.repeat} /> */}
+                  <LocationDetail
+                    id={data.task._id}
+                    location={data.task.location}
+                  />
+                  <BodyDetail id={data.task._id} body={data.task.body} />
+                </div>
               </div>
             </div>
-          </div>
-        );
-      }}
-    </Query>
-  );
-};
+          );
+        }}
+      </Query>
+    );
+  }
+}
 
 export default withRouter(TaskShow);
