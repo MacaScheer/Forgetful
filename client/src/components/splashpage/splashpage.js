b1cb440a4d3436b3493693247275a14412e99978
Michael_Scheer <macas.scheer@gmail.com>
2019-10-09 20:26:25 -0400

    add background image remove deadlinks

--- client/src/components/splashpage/splashpage.js
+++ client/src/components/splashpage/splashpage.js
@@ -14,14 +14,15 @@ class SplashNavbar extends React.Component {
       //     "https://static.parade.com/wp-content/uploads/2016/06/findingdory568c691272fd3-72.jpg"
       //   }
       // />
+      <div className="splash-background">
       <div className="home-page-div-tags">
         <div className="splash-intro-prewords">Welcome to</div>
         <div className="splash-intro">Forgetful</div>
-        <img className="logo" src={require("./favicon1.ico")} />
         {/* <div className="splash-description">We save ur tasks to different lists so you will never forget what you need to do!</div> */}
         <div className="splash-description">
           {" "}
           a task management application
+        </div>
         </div>
       </div>
     );
